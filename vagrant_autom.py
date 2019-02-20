75f2c89a7c127dffbc8957c916fceb0c72149f4a
Hector <hecmar.bcn@gmail.com>
2019-02-20 22:08:36 +0100

    Primer update con GIT

--- vagrant_autom.py
+++ vagrant_autom.py
@@ -6,6 +6,7 @@
 
 import os
 
+
 # Variables
 
 name_OS = str("")
@@ -14,6 +15,7 @@ menu_network = int(0)
 ram= int(0)
 ip = str("")
 name_network = str("")
+
 
 # Funtions
 
@@ -62,14 +64,17 @@ def internal_network():
     ip = input("\nWhat IP do you want?\n> ")
     name_opt = input("\nNeed you a specific name for the private network? (yes|no)\n> ")
     if name_opt == "yes":
-        name_network = input("\nWhat name do you want?\n> ");
+        name_network = input("\nWhat name do you want?\n> ")
+    elif name_opt == "no":
+        name_network = "true"
+    
 
 def hostO_static():
     '''Conf Host-Only (static) network interface'''
     global ip
     ip = input("What IP do you want?\n> ")
 
-def write_Vagrantfile(name_OS):
+def write_Vagrantfile(me_OS,hostname,menu_network,ip,name_network,ram):
     '''This funtion writes in Vagrantfile with variables that has been recolected'''
     try:
         file = open("Vagrantfile", "w")
@@ -77,9 +82,21 @@ def write_Vagrantfile(name_OS):
         print("no se encuentra el archivo")
 
     file.write('Vagrant.configure("2") do |config|' + os.linesep)
-    file.write('"config.vm.box = "' + name_OS + '"')
+    file.write('    config.vm.box = "' + name_OS + '"' + os.linesep)
+    file.write('    config.vm.hostname = "' + hostname + '"' + os.linesep)
     file.write(os.linesep)
+    if menu_network == '1':
+        file.write('    config.vm.network "private_network", ip: "' + ip + '",' + os.linesep)
+        file.write('    virtualbox__intnet: ' + name_network + os.linesep)
+    elif menu_network == '2':
+        file.write('    config.vm.network "private_network", ip: "' + ip + '"' + os.linesep)
+    file.write('    config.vm.provider "virtualbox" do |vb|' + os.linesep)
+    file.write('        vb.memory = "' + ram + '"' + os.linesep)
+    file.write('    end' + os.linesep)
+    file.write('end')
+
     file.close()
+
 
 # Structure
 
@@ -89,28 +106,19 @@ name_OS = input("\nWhat operating sistem do you want? \n> ")  # Name OS
 hostname = input("\nWhat hostname do you want? \n> ")  # Hostname
 menu_network = menu_network_box()  # menu to select network
 
-# network
+# networfile.write('hola')
 if menu_network == "1":  # if select internal network
     internal_network()
 elif menu_network == "2":  # if select Host-Only with static IP
     hostO_static() 
+print(menu_network)
 
-ram = input("\nHow much RAM do you want? \n> ")
+ram = input("\nHow much RAM do you want?(autoselect: 1024) \n> ")
+if ram == 0:
+    ram = 1024
+write_Vagrantfile(name_OS,hostname,menu_network,ip,name_network,ram)
 
-write_Vagrantfile(name_OS)
 print("**END**")
 
 
 # name_os , hostname , menu_network , ip , name_network , ram
-
-
-
-
-
-
-
-
-
-
-
-
